6cc28c2969d80b8fa17955c715537dafb72c6698
Twoje Imię <twoj@email.com>
2025-10-29 08:51:09 +0100

    Poprawa styli i struktury HTML

--- Routing.php
+++ Routing.php
@@ -1,19 +1,38 @@
 <?php
 require_once 'src/controllers/SecurityController.php';
+require_once 'src/controllers/DashboardController.php';
 
 class Routing {
+    public static $routes=[
+        'login'=>[
+            'controller'=>'SecurityController',
+            'action'=>'login'
+        ],
+        'register'=>[
+            'controller'=>'SecurityController',
+            'action'=>'register'
+        ],
+
+        'dashboard'=>[
+            'controller'=>'DashboardController',
+            'action'=>'index'
+        ],
+        
+    ];
 
     public static function run(string $path) {
         //TODO na podstawie sciezki sprawdzamy jaki HTML zwrocic
-        switch ($path) {
+    switch ($path) {
     case 'dashboard':
-        include 'public/views/dashboard.html';
-        break;
     case 'login':
-        $controller=new SecurityController();
-        $controller->login();
-        // include 'public/views/login.html';
+    case 'register':
+        
+        $controller=Routing::$routes[$path]['controller'];
+        $action=Routing::$routes[$path]['action'];
+        $controllerObj=new $controller;
+        $controllerObj->$action();
         break;
+
     default:
         include 'public/views/404.html';
         break;
