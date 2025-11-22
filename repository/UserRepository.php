9acf3b176dfd23e8cd2b458f46bd92eed5cf3fb1
dromeczek <dromeczek2004@gmail.com>
2025-11-22 23:11:56 +0100

    naprawienie polaczenia z baza

--- repository/UserRepository.php
+++ repository/UserRepository.php
@@ -1,6 +1,8 @@
 <?php
+require_once __DIR__ . '/repository.php';
 
-require_once 'Repository.php';
+
+
 //require_once __DIR__.'/../models/User.php';
 
 class UserRepository extends Repository
--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -1,6 +1,10 @@
+
+
 <?php
+require_once 'repository/UserRepository.php';
 require_once 'AppController.php';
-require_once __DIR__ . '/../repository/UserRepository.php';
+
+
 
 class DashboardController extends AppController{
 
